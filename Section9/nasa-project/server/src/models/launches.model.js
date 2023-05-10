7ef91271cbc02fc22b0c4ed039165ed549086426
Dong-Yi-Xia <dong7777xia@gmail.com>
2023-05-09 17:38:04 -0700

    addNewLaunch

--- Section9/nasa-project/server/src/models/launches.model.js
+++ Section9/nasa-project/server/src/models/launches.model.js
@@ -3,15 +3,17 @@
 // Map is more flexible than js objects, key and value can be any
 const launches = new Map()
 
+let latestFlightNumber = 100
+
 const launch = {
-    flightNumber : 100,
     mission: 'Mission name 100',
     rocket: 'Rocket name 100',
     launchDate: new Date('December 27, 2030'),
     target: 'Kepler-1652 b',
-    customers: ['ABC', 'NASA'],
-    upcoming: true,
-    success: true,
+    // flightNumber : 100,
+    // customers: ['ABC', 'NASA'],
+    // upcoming: true,
+    // success: true,
 }
 
 launches.set(launch.flightNumber, launch)
@@ -20,6 +22,21 @@ function getAllLaunches() {
     return Array.from(launches.values())
 }
 
+function addNewLaunch(launch){
+    latestFlightNumber++
+    launches.set(
+        latestFlightNumber,
+        Object.assign(launch, {
+            //have it be server side rather than getting from client side, reduce client from sending unnecessary data
+            flightNumber: latestFlightNumber,
+            customers: ['ZTM', 'NASA'],
+            upcoming: true,
+            success: true,
+        })
+    )
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 }
